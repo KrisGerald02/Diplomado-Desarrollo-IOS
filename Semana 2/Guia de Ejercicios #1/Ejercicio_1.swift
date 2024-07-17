2c0a94aa7638d946b5a58dc3e569c2f7c9f69f26
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-07-16 22:53:19 -0600

    Update Ejercicio_1.swift

--- Semana 2/Guia de Ejercicios #1/Ejercicio_1.swift
+++ Semana 2/Guia de Ejercicios #1/Ejercicio_1.swift
@@ -1,1 +1,13 @@
+//Ejercicio 1: Calcular el área y el perímetro de un rectángulo
+import Foundation
+//Funcion calcular el area del perimetro
+func calcularAreaYPerimetro(ancho: Double, alto: Double) -> (area: Double, perimetro: Double) {
+    let area = ancho * alto
+    let perimetro = 2 * (ancho + alto)
+    return (area, perimetro)
+}
 
+//Ejecucion
+let resultado = calcularAreaYPerimetro(ancho: 5, alto: 10)
+print("Área: \(resultado.area)")
+print("Perímetro: \(resultado.perimetro)")
