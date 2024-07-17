622b7ab8950fec04e9fcc9c76a44a334e82b28c9
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-07-16 22:47:26 -0600

    Create Ejercicio_5.swift

---
+++ Semana 3/Guia_Ejercicios_3/Ejercicio_5.swift
@@ -0,0 +1,47 @@
+//Ejercicio 5: Clases, Structs y Polimorfismo
+// Protocolo Empleado
+protocol Empleado {
+    func calcularSalario() -> Double
+}
+
+// Clase EmpleadoBase
+class EmpleadoBase: Empleado {
+    var salarioBase: Double
+    
+    init(salarioBase: Double) {
+        self.salarioBase = salarioBase
+    }
+    
+    func calcularSalario() -> Double {
+        return salarioBase
+    }
+}
+
+// Struct EmpleadoContrato
+struct EmpleadoContrato: Empleado {
+    var tarifaHora: Double
+    var horasTrabajadas: Double
+    
+    func calcularSalario() -> Double {
+        return tarifaHora * horasTrabajadas
+    }
+}
+
+// Función para calcular el salario total
+func calcularSalarioTotal(empleados: [Empleado]) -> Double {
+    var salarioTotal: Double = 0.0
+    
+    for empleado in empleados {
+        salarioTotal += empleado.calcularSalario()
+    }
+    
+    return salarioTotal
+}
+
+// Ejecucion
+let empleadoBase = EmpleadoBase(salarioBase: 3000.0)
+let empleadoContrato = EmpleadoContrato(tarifaHora: 20.0, horasTrabajadas: 100.0)
+let empleados: [Empleado] = [empleadoBase, empleadoContrato]
+
+let salarioTotal = calcularSalarioTotal(empleados: empleados)
+print("Salario total: \(salarioTotal)")
