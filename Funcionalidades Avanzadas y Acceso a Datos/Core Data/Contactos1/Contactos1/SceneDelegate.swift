db9def81ffe2b0630d46b9695a1d82fc3d8d7042
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-12-01 17:12:31 -0600

    Add files via upload

---
+++ Funcionalidades Avanzadas y Acceso a Datos/Core Data/Contactos1/Contactos1/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  Contactos1
+//
+//  Created by Geraldine on 12/1/24.
+//
+
+import UIKit
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
